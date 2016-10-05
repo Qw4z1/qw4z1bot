88a2a237a8bd3c54130e09d68f9ccc3e36a1e046
Viktor Nyblom <viktor.nyblom@gmail.com>
2016-10-05 20:08:09 +0200

    Test

--- src/lib/messenger.js
+++ src/lib/messenger.js
@@ -9,6 +9,7 @@ const inputParser = new InputParser();
 export default class Messenger {
 
   constructor() {
+    console.log("Tests");
     if (process.env.NODE_ENV === 'production') {
       this.bot = new TelegramBot(config.telegram.token, { webHook: { port: config.telegram.port, host: config.telegram.host } });
       this.bot.setWebHook(config.telegram.externalUrl + ':443/bot' + config.telegram.token);
