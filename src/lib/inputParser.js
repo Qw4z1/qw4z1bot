0da062cbc02bc14b2e2e2d318a231fc6dbf46ae7
w1nston <wallander.jonas@gmail.com>
2016-10-17 11:01:08 +0200

    Fix incorrect assumption about includes.

--- src/lib/inputParser.js
+++ src/lib/inputParser.js
@@ -2,24 +2,24 @@ export function isAskingForGreeting(text) {
   return '/start'.includes(text);
 }
 
-export function isAskingForBone(text) {
-  return '/böne'.includes(text);
+export function isAskingForBone(text = '') {
+  return text.includes('/böne');
 }
 
-export function isAskingForMartinez(text) {
-  return '/martinez'.includes(text);
+export function isAskingForMartinez(text = '') {
+  return text.includes('/martinez');
 }
 
-export function isAskingForBronas(text) {
-  return '/bronas'.includes(text);
+export function isAskingForBronas(text = '') {
+  return text.includes('/bronas');
 }
 
-export function isAskingForBoegivar(text) {
-  return '/boegivar'.includes(text);
+export function isAskingForBoegivar(text = '') {
+  return text.includes('/boegivar');
 }
 
-export function isAskingForTourettes(text) {
-  return '/tourettes'.includes(text);
+export function isAskingForTourettes(text = '') {
+  return text.includes('/tourettes');
 }
 
 export function isAskingForRandom(text) {
